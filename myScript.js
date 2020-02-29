cea431ab26298ea2ce559ce26d797a7063998516
tran6christina <55112745+tran6christina@users.noreply.github.com>
2020-02-28 19:09:44 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,3 +2,15 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+function changeText(text) {
+  var display = document.getElementById('text-display');
+  display.innerHTML = "";
+  display.innerHTML = text;
+}
+
+function changeback(text) {
+  var display = document.getElementById('text-display');
+  display.innerHTML = "";
+  display.innerHTML = text;
+}
